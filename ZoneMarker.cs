b0c23c5d7d03a80760f4200364396e049bc7bfa1
sh4 <sh4@users.noreply.github.com>
2017-01-26 02:08:29 +0900

    Initial commit.

---
+++ ZoneMarker.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services.Navigation;
+using JetBrains.Util;
+using JetBrains.Util.Logging;
+
+namespace ReSharper.RpCorrector
+{
+    [ZoneMarker]
+    public class ZoneMarker
+    {
+    }
+}
