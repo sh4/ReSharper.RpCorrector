4f46cff51a0ce2551c68c58c218f74cb5ca94257
sh4 <sh4@users.noreply.github.com>
2017-02-27 23:37:43 +0900

    Add disable comment supports.

--- RpCorrector/XamlMissingReactivePropertyValueHighlighting.cs
+++ RpCorrector/XamlMissingReactivePropertyValueHighlighting.cs
@@ -3,14 +3,28 @@ using JetBrains.ReSharper.Feature.Services.Daemon;
 using JetBrains.ReSharper.Psi;
 using JetBrains.ReSharper.Psi.Xaml.Impl.Util;
 using JetBrains.ReSharper.Psi.Xaml.Tree;
+using ReSharper.RpCorrector;
+
+[assembly: RegisterConfigurableSeverity(
+    XamlMissingReactivePropertyValueHighlighting.HIGHLIGHTING_ID,
+    "XAML",
+    "XAMLErrors",
+    XamlMissingReactivePropertyValueHighlighting.MESSAGE,
+    "Missing ReactiveProperty's '.Value' field or property.",
+    Severity.ERROR)]
 
 namespace ReSharper.RpCorrector
 {
-    [StaticSeverityHighlighting(Severity.ERROR, 
-        "XAMLErrors", 
-        AttributeId = "ReSharper Error Underlined")]
-    public class XamlMissingReactivePropertyValueHighlighting : XamlResolveErrorHighlighting
+    [ConfigurableSeverityHighlighting(HIGHLIGHTING_ID, "XAML",
+        AttributeId = "ReSharper Error Underlined",
+        Languages = "XAML",
+        OverlapResolve = OverlapResolveKind.ERROR,
+        ToolTipFormatString = MESSAGE)]
+    public class XamlMissingReactivePropertyValueHighlighting : XamlResolveErrorHighlightingBase
     {
+        public const string MESSAGE = "'.Value' is not specified ReactiveProperty field or property '{0}'";
+        public const string HIGHLIGHTING_ID = "Xaml.MissingReactivePropertyValue";
+
         public XamlMissingReactivePropertyValueHighlighting(IMarkupPropertyReference reference)
             : base(reference, CreateTooltip(reference))
         {
@@ -21,7 +35,7 @@ namespace ReSharper.RpCorrector
             var languageType = ReferenceUtil.GetProjectLanguage(reference.GetTreeNode());
             if (languageType.IsNullOrUnknown())
                 return $"Cannot resolve symbol '{reference.GetName()}'";
-            return $"'.Value' is not specified ReactiveProperty field or property '{reference.GetName()}'";
+            return string.Format(MESSAGE, reference.GetName());
         }
     }
 }
