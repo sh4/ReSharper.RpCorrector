a44bcc2ffe103b464a29183daf3f8366cf110c66
sh4 <sh4@users.noreply.github.com>
2017-02-28 01:31:05 +0900

    Divided into .csproj R# 2017.1 and 2016.2

---
+++ RpCorrector/ZoneMarker.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Psi.Xaml;
+
+namespace ReSharper.RpCorrector
+{
+    [ZoneMarker]
+    public class ZoneMarker : IRequire<DaemonZone>, IRequire<ILanguageXamlZone>
+    {
+        
+    }
+}
